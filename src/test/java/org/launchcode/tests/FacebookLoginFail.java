79359e38ef464d2f25dda612cfd417ba97be7af1
bhavana1308 <bonamukkala.bhavana@gmail.com>
2024-07-29 11:45:32 -0500

    Pom fb example tests with dataprovider

---
+++ src/test/java/org/launchcode/tests/FacebookLoginFail.java
@@ -0,0 +1,50 @@
+package org.launchcode.tests;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.launchcode.pages.LoginPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class FacebookLoginFail {
+    private WebDriver driver;
+    private LoginPage loginPage;
+
+    @BeforeMethod
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://www.facebook.com");
+        loginPage = new LoginPage(driver);
+    }
+
+    @Test(dataProvider = "invalidCredentials")
+    public void testInvalidLogin(String email, String password) {
+        loginPage.enterEmail(email);
+        loginPage.enterPassword(password);
+        loginPage.clickLogin();
+
+        // Assert the presence of the error message
+        Assert.assertTrue(loginPage.isErrorTextDisplayed(), "The error message was not displayed as expected.");
+    }
+
+    @DataProvider(name = "invalidCredentials")
+    public Object[][] getInvalidCredentials() {
+        return new Object[][]{
+                {"invalid_user@example.com", "wrongpassword"},
+                {"another_invalid@example.com", "123456"}
+        };
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
