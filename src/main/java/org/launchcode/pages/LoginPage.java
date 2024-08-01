41c758d76b2e3451335ebd3d19091eab057be725
bhavana1308 <bonamukkala.bhavana@gmail.com>
2024-08-01 09:58:31 -0500

    page factory

--- src/main/java/org/launchcode/pages/LoginPage.java
+++ src/main/java/org/launchcode/pages/LoginPage.java
@@ -1,41 +1,44 @@
 package org.launchcode.pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
 
     private final WebDriver driver;
 
+    @FindBy(id = "email")
+    private WebElement emailField;
 
-    private final By emailField = By.id("email");
-    private final By passwordField = By.id("pass");
-    private final By loginButton = By.name("login");
-    private final By errorText = By.xpath("//div[@id='email_container']//div[contains(text(), 'The email address you entered')]");
+    @FindBy(id = "pass")
+    private WebElement passwordField;
 
+    @FindBy(name = "login")
+    private WebElement loginButton;
+
+    @FindBy(xpath = "//div[@id='email_container']//div[contains(text(), 'The email address you entered')]")
+    private WebElement errorText;
 
     public LoginPage(WebDriver driver) {
         this.driver = driver;
+        PageFactory.initElements(driver, this);
     }
 
-
     public void enterEmail(String email) {
-        driver.findElement(emailField).sendKeys(email);
+        emailField.sendKeys(email);
     }
 
     public void enterPassword(String password) {
-        driver.findElement(passwordField).sendKeys(password);
+        passwordField.sendKeys(password);
     }
 
     public void clickLogin() {
-        driver.findElement(loginButton).click();
+        loginButton.click();
     }
 
     public boolean isErrorTextDisplayed() {
-        WebElement errorElement = driver.findElement(errorText);
-        return errorElement.isDisplayed();
+        return errorText.isDisplayed();
     }
-
 }
-
